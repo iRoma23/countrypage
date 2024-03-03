567524c31cb702cf1ac3a0a6d24e5d1049db531e
Romario <romario.jara.ch@gmail.com>
2024-03-02 21:25:37 -0500

    feat: add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#282B30",
+          200: "#1B1D1F",
+        },
+        blue: {
+          base: "#4E80EE",
+        },
+        gray: {
+          base: "#6C727F",
+        },
+        white: {
+          base: "#D2D5DA",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
