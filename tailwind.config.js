56c03cf60ad603fb34ac87d3f5a3ed5f26441c10
Romario <romario.jara.ch@gmail.com>
2024-03-12 19:46:28 -0500

    update color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          100: "#282B30",
-          200: "#1B1D1F",
-        },
-        blue: {
-          base: "#4E80EE",
-        },
-        gray: {
-          base: "#6C727F",
-        },
-        white: {
-          base: "#D2D5DA",
-        },
+    extend: {},
+    colors: {
+      primary: "#1B1D1F",
+      secondary: "#282B30",
+      blue: {
+        base: "#4E80EE",
+      },
+      gray: {
+        base: "#6C727F",
+      },
+      white: {
+        base: "#D2D5DA",
       },
     },
     fontFamily: {
