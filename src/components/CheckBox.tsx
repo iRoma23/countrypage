b133f8403ff57f567cad5cc9f6e6dbfe7273737b
Romario <romario.jara.ch@gmail.com>
2024-03-14 19:53:03 -0500

    create CheckBox component

---
+++ src/components/CheckBox.tsx
@@ -0,0 +1,22 @@
+type Props = {
+  children: string;
+  isActive: boolean;
+  handleCheckBox: () => void;
+};
+
+function CheckBox({ children, isActive, handleCheckBox }: Props) {
+  return (
+    <label className="mt-3 flex w-max cursor-pointer items-center text-sm font-bold text-white-base">
+      <input
+        type="checkbox"
+        className={`mr-3 size-6 appearance-none rounded-md border-2 border-gray-base bg-primary bg-center checked:border-blue-base checked:bg-blue-base
+        ${isActive ? "bg-[url('./assets/Done_round.svg')]" : ""}`}
+        onChange={handleCheckBox}
+        checked={isActive}
+      />
+      {children}
+    </label>
+  );
+}
+
+export default CheckBox;
