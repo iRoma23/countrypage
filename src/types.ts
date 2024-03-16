b2d9ef8470a9ca07e888c04e293e8268786f48f0
Romario <romario.jara.ch@gmail.com>
2024-03-16 01:30:44 -0500

    create types

---
+++ src/types.ts
@@ -0,0 +1,35 @@
+export type Country = {
+  flags: Flags;
+  name: Name;
+  independent: boolean;
+  unMember: boolean;
+  area: number;
+  population: number;
+};
+
+type Flags = {
+  png: string;
+  svg: string;
+  alt: string;
+};
+
+type Name = {
+  common: string;
+  official: string;
+  nativeName: NativeName;
+};
+
+type NativeName = {
+  ell: Ell;
+  tur: Tur;
+};
+
+type Ell = {
+  official: string;
+  common: string;
+};
+
+type Tur = {
+  official: string;
+  common: string;
+};
