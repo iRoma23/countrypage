eb09976bc5a28114d4d49d22f7ad74d0449f1487
Romario <romario.jara.ch@gmail.com>
2024-03-16 01:54:44 -0500

    create countries services

---
+++ src/services/countries.ts
@@ -0,0 +1,13 @@
+import { Country } from "../types";
+
+const url =
+  "https://restcountries.com/v3.1/all?fields=name,flags,area,population,independent,unMember";
+
+const getAll = async (): Promise<Country[]> => {
+  const response = await fetch(url);
+  if (!response.ok) throw new Error(response.statusText);
+  const data = await response.json();
+  return data;
+};
+
+export default { getAll };
