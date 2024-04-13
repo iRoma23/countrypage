0f57b4a6743b6aed2a1663ae04dd224d7c788d5d
Romario <romario.jara.ch@gmail.com>
2024-04-13 01:36:05 -0500

    add handleToggle prop

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,17 @@
 type Props = {
   children: string;
   isActive: boolean;
+  handleToogle: () => void;
 };
 
-function Button({ children, isActive }: Props) {
+function Button({ children, isActive, handleToogle }: Props) {
   return (
     <button
       className={`
       rounded-xl px-3 py-1.5 text-sm font-bold
       ${isActive ? "bg-secondary text-white-base" : "bg-primary text-gray-base"}
       `}
+      onClick={handleToogle}
     >
       {children}
     </button>
