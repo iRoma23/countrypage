4a7637bd11e583888a8e810407c10aea75d28dda
Romario <romario.jara.ch@gmail.com>
2024-04-15 01:25:53 -0500

    delete comments

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,23 +2,6 @@ import logoSVG from "../assets/Logo.svg";
 
 function Header() {
   return (
-    // <header className="relative mt-0 h-60 w-screen bg-black pt-8">
-    //   <img
-    //     className="absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%]"
-    //     src={logoSVG}
-    //     alt="World Ranks logo image"
-    //   />
-    //   <div className="h-52 bg-[url('./assets/hero-image-wr.jpg')] bg-top"></div>
-    // </header>
-    // <header className="relative h-[18.75rem] w-screen bg-black">
-    //   <img
-    //     className="absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-100%]"
-    //     src={logoSVG}
-    //     alt="World Ranks logo image"
-    //   />
-    //   <div className="h-[18.75rem] bg-[url('./assets/hero-image-wr.jpg')] bg-top"></div>
-    // </header>
-
     <header className="relative -z-10 h-60 bg-[#000] pt-11 sm:h-60 sm:pt-11 lg:h-[18.75rem]">
       <img
         className="absolute left-1/2 top-1/2 translate-x-[-50%]"
