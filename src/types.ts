18feb32dcad9901e0603b742b911fff03298e752
Romario <romario.jara.ch@gmail.com>
2024-04-15 17:45:46 -0500

    add raw data country type

--- src/types.ts
+++ src/types.ts
@@ -1,25 +1,8 @@
-export type Country = {
-  flags: Flags;
-  name: Name;
-  independent?: boolean;
-  unMember: boolean;
-  area: number;
-  population: number;
-  region: Region;
-  subregion: string;
-};
-
-type Flags = {
-  png: string;
-  svg: string;
-  alt: string;
-};
-
-type Name = {
-  common: string;
-  official: string;
-  nativeName: object;
-};
+export enum SortFilter {
+  Alphabetical = "alphabetical",
+  Population = "population",
+  Area = "area",
+}
 
 type Region =
   | "Americas"
@@ -29,8 +12,52 @@ type Region =
   | "Oceania"
   | "Antarctic";
 
-export enum SortFilter {
-  Alphabetical = "alphabetical",
-  Population = "population",
-  Area = "area",
+type FlagsRawData = {
+  png: string;
+  svg: string;
+  alt: string;
+};
+
+type NameRawData = {
+  common: string;
+  official: string;
+  nativeName: object;
+};
+
+interface BaseCountryRawData {
+  flags: FlagsRawData;
+  name: NameRawData;
+  area: number;
+  population: number;
+  subregion: string;
+}
+
+export interface CountryRawData extends BaseCountryRawData {
+  independent?: boolean;
+  unMember: boolean;
+  region: Region;
+}
+
+type Flags = {
+  png: string;
+  alt: string;
+};
+
+type Name = {
+  common: string;
+  official: string;
+};
+
+interface BaseCountry {
+  flags: Flags;
+  name: Name;
+  area: number;
+  population: number;
+  subregion: string;
+}
+
+export interface Country extends BaseCountry {
+  independent?: boolean;
+  unMember: boolean;
+  region: Region;
 }
