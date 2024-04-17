b7d7608a2c560c9df4eab3e1b6699fcc1ad40a7a
Romario <romario.jara.ch@gmail.com>
2024-04-17 02:05:24 -0500

    create fetch data function by cca3

--- src/services/countries.ts
+++ src/services/countries.ts
@@ -1,9 +1,11 @@
 // import { Country } from "../types";
-import { CountryRawData } from "../types";
+import { CountryDetailsRawData, CountryRawData } from "../types";
 
 const baseUrl = "https://restcountries.com/v3.1";
 const allCountriesParams =
   "fields=name,flags,area,population,region,subregion,independent,unMember";
+const countryParams =
+  "fields=name,capital,subregion,languages,currencies,continents,borders,population,area,flags";
 
 const getAll = async (): Promise<CountryRawData[]> => {
   const response = await fetch(`${baseUrl}/all?${allCountriesParams}`);
@@ -12,4 +14,11 @@ const getAll = async (): Promise<CountryRawData[]> => {
   return data;
 };
 
-export default { getAll };
+const getByCca3 = async (cca3: string): Promise<CountryDetailsRawData> => {
+  const response = await fetch(`${baseUrl}/alpha/${cca3}?${countryParams}`);
+  if (!response.ok) throw new Error(response.statusText);
+  const data = await response.json();
+  return data;
+};
+
+export default { getAll, getByCca3 };
