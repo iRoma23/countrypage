38e16fdcd87949bdc6d1683bfb7521276b986e7b
Romario <romario.jara.ch@gmail.com>
2024-04-18 19:09:54 -0500

    create a util, array of strings to string

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -2,3 +2,9 @@ export const capitalizeFirstLetter = (word: string): string => {
   const capitalized = word.charAt(0).toUpperCase() + word.slice(1);
   return capitalized;
 };
+
+export const arrayToString = (arr: string[]): string => {
+  return arr
+    .map((element, i) => (i === 0 ? element : " " + element))
+    .toString();
+};
