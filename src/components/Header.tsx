98c93c404c590081d1fe063b835c0c5a5fa7dfc7
Romario <romario.jara.ch@gmail.com>
2024-04-18 19:58:46 -0500

    update background header design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,13 @@ import logoSVG from "../assets/Logo.svg";
 
 function Header() {
   return (
-    <header className="relative -z-10 h-60 bg-[#000] pt-11 sm:h-60 sm:pt-11 lg:h-[18.75rem]">
+    <header className="relative -z-10 h-60 bg-[#000] pt-11 lg:h-[18.75rem]">
       <img
         className="absolute left-1/2 top-1/2 translate-x-[-50%]"
         src={logoSVG}
         alt="World Ranks logo image"
       />
-      <div className="h-[12.25rem] bg-[url('./assets/hero-image-wr.jpg')] bg-top sm:h-[12.25rem]"></div>
+      <div className="h-[12.25rem] bg-[url('./assets/hero-image-wr.jpg')] bg-top bg-no-repeat lg:h-64 lg:bg-center"></div>
     </header>
   );
 }
