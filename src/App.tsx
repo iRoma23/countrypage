bfbb0d29a359851ee5510ffcdaba5b417373c956
Romario <romario.jara.ch@gmail.com>
2024-04-19 19:45:02 -0500

    add routes to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
+import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Country } from "./types";
+
+import countryService from "./services/countries";
+
+import { parseCountries } from "./utils/helper";
+
 import Header from "./components/Header";
-import Main from "./components/Main";
+import CountryListPage from "./pages/page";
+import CountryPage from "./pages/countries/page";
 
 function App() {
+  const [countries, setCountries] = useState<Country[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchCountryList = async () => {
+      setIsLoading(true);
+      try {
+        const data = await countryService.getAll();
+        const parsedData = parseCountries(data);
+        setCountries(parsedData);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    void fetchCountryList();
+  }, []);
+
   return (
     <>
       <Header />
-      <Main />
+      <Routes>
+        <Route
+          path="/countries/:cca3"
+          element={<CountryPage countries={countries} />}
+        />
+        <Route
+          path="/"
+          element={
+            <CountryListPage countries={countries} isLoading={isLoading} />
+          }
+        />
+      </Routes>
     </>
   );
 }
